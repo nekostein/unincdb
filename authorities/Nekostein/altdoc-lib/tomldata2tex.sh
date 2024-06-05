2a32c4052e9ae1bfcabb9a78a5b371449ca5f5e7
Neruthes <i@neruthes.xyz>
2024-06-05 07:57:15 +0000

    Regular update '2024-06-05T07:57:15+00:00'

--- authorities/Nekostein/altdoc-lib/tomldata2tex.sh
+++ authorities/Nekostein/altdoc-lib/tomldata2tex.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
 
-# fullname = "SAMPLE DATA FOR UNINCDB TUTORIAL"
-# type = "Quasi-LLC"
-# date_creation = "1970-01-01"
-# status = "Active"
-# fields = "Information Technology; Witness Service"
-# president = "NERUTHES"
-# secretary = "NERUTHES"
-# charter_hash = "633251ca87f8b59d1c986eb508d9b22634dacf02"
-# addresses = [
-#     "https://github.com/nekostein/unincdb",
-# ]
-# witness = [
-#     "https://unincdb.nekostein.com/1970/unincdb-tutorial.pdf"
-# ]
 
 keys="fullname type date_creation status fields president secretary charter_hash"
 
@@ -27,6 +13,9 @@ done
 for keyname in addresses witness; do
     printf '\\providecommand{\\uninctomldataAT%sITEM}{%s}\n' "$keyname" "$(
         tomlq -r ".$keyname[]" .workdir/UNINC.toml | sed 's|^|\\item |'
+    )"
+    printf '\\providecommand{\\uninctomldataAT%sFIRST}{%s}\n' "$keyname" "$(
+        tomlq -r ".$keyname[0]" .workdir/UNINC.toml
     )"
 done
 
--- authorities/Nekostein/prealtdoc.sh
+++ authorities/Nekostein/prealtdoc.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 bash "authorities/$OFFICE/altdoc-lib/tomldata2tex.sh" > .workdir/tomldata2tex.texpart
+
+tomlq -r '[paths | map(tostring) | join(".")][]' "$toml" | grep -vE '[0-9]' > .workdir/tomlkeys.txt
+while read -r tomlkey; do
+    tomlkey_flat="$(sed 's|\.|DOT|g' <<< "$tomlkey" | sed 's|_||g')"
+    toml_value="$(tomlq -r ."$tomlkey" "$toml")"
+    char0="${toml_value:0:1}"
+    if [[ "$char0" != "{" ]] && [[ "$char0" != "[" ]]; then
+        printf '\\providecommand{\\fulltomldataAT%s}[0]{%s}\n' "$tomlkey_flat" "$toml_value"
+    fi
+done < .workdir/tomlkeys.txt > .workdir/fulltomldata.texpart
+
