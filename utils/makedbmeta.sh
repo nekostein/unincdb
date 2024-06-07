a8f39b7e20fa3c4853adcb76a89567231f6b2840
Neruthes <i@neruthes.xyz>
2024-06-07 04:54:51 +0000

    Regular update '2024-06-07T04:54:51+00:00'

--- utils/makedbmeta.sh
+++ utils/makedbmeta.sh
@@ -7,7 +7,9 @@ source .myenv
 
 function process_toml_path() {
     toml_path="$1/UNINC.toml"
-    URL="$(cut -d/ -f2-3 <<< "$1")"
+    # URL="$(cut -d/ -f2-3 <<< "$1")"
+    URL="$(bash utils/helper-transformpdfpath.sh "$1/witness-$OFFICE.pdf" | cut -d/ -f4-)"
+    [[ ! -e $toml_path ]] && return 0
 
     echo -n '\bizinfo'
 
@@ -26,8 +28,10 @@ mkdir -p meta
 LIST="authorities/$OFFICE/witnesslist.txt"
 OUT="authorities/$OFFICE/dblist.texpart"
 
-sort -r "$LIST" -o "$LIST".new
-mv "$LIST".new "$LIST"
+
+### Do we really need the sorting?
+# sort -r "$LIST" -o "$LIST".new
+# mv "$LIST".new "$LIST"
 
 
 
@@ -36,9 +40,9 @@ years="$(cut -d/ -f2 "$LIST" | sort -ru)"
 
 for year in $years; do
     printf '\n\n\\thisyear{%s}\n\n' "$year"
-    grep "^db/$year/" "$LIST" | while read -r line; do
+    grep "^db/$year/" "$LIST" | cut -d';' -f1 | while read -r line; do
         process_toml_path "$line"
     done
-done  > "$OUT"
+done > "$OUT"
 
 
