4e7dc8a987d4e120547a11c2db81a5c1dd393d82
Neruthes <i@neruthes.xyz>
2024-06-09 01:25:46 +0000

    Regular update '2024-06-09T01:25:46+00:00'

--- authorities/Nekostein/altdoc-lib/fulltomldata.sh
+++ authorities/Nekostein/altdoc-lib/fulltomldata.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+
+
+tomlkeyi18nprefix="."
+if [[ -n "$TOML_I18N_PREFER" ]]; then
+    if [[ "$(tomlq -r ".i18n.$TOML_I18N_PREFER" "$toml")" != null ]]; then
+        tomlkeyi18nprefix=".i18n.$TOML_I18N_PREFER."
+    fi
+fi
+
+
 tomlq -r '[paths | map(tostring) | join(".")][]' "$toml" | grep -vE '[0-9]' > .workdir/tomlkeys.txt
 while read -r tomlkey; do
     tomlkey_flat="$(sed 's|\.|DOT|g' <<< "$tomlkey" | sed 's|_||g')"
-    toml_value="$(tomlq -r ."$tomlkey" "$toml")"
-    char0="${toml_value:0:1}"
-    if [[ "$char0" != "{" ]] && [[ "$char0" != "[" ]]; then
-        printf '\\providecommand{\\fulltomldataAT%s}[0]{%s}\n' "$tomlkey_flat" "$toml_value"
-    fi
+    toml_value="$(tomlq -r "${tomlkeyi18nprefix}${tomlkey}" "$toml")"
+    case "$(tomlq -r "${tomlkeyi18nprefix}${tomlkey} | type" "$toml")" in
+        'object')
+            printf ''
+            ;;
+        'array')
+            printf '\\providecommand{\\fulltomldataAT%s}[0]{%s}\n' "$tomlkey_flat" "$(tomlq -r "${tomlkeyi18nprefix}${tomlkey}[]" "$toml")"
+            ;;
+        'string')
+            printf '\\providecommand{\\fulltomldataAT%s}[0]{%s}\n' "$tomlkey_flat" "$toml_value"
+            ;;
+    esac
 done < .workdir/tomlkeys.txt
 
 printf '\\providecommand{\\unincdbaltdocprefix}[0]{https://unincdb.nekostein.com/%s}\n' "$(grep "^$ORGDIR;" "authorities/$OFFICE/witnesslist.txt" | cut -d';' -f2)"
